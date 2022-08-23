342b38429c732345d1935b3e581978b5c31cefe1
Chagalla <hchagalla@gmail.com>
2022-08-23 23:51:28 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
-# Rust Smart Contract Template
+# Library App smart contract rust
 
-## Getting started
+## About
 
-To get started with this template:
+This is a library_app project to automate the library services meant for a library with many users making it easier to accommodate each user's needs and locating books with ease.
 
-1. Click the "Use this template" button to create a new repo based on this template
-2. Update line 2 of `Cargo.toml` with your project name
-3. Update line 4 of `Cargo.toml` with your project author names
-4. Set up the [prerequisites](https://github.com/near/near-sdk-rs#pre-requisites)
-5. Begin writing your smart contract in `src/lib.rs`
-6. Test the contract 
+### Initialization for dependencies needed
 
-    `cargo test -- --nocapture`
+`use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::Vector;
+use near_sdk::{env, near_bindgen};`
 
-8. Build the contract
+- Write your smart contract in `src\lib.rs`
+- Test the contract
+
+     `cargo test -- --nocapture`
+
+- Build the contract
 
     `RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release`
+
+Built with the Near Rust Template    
 
 **Get more info at:**
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,196 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::{near_bindgen};
 
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+enum BookCategories {
+    Any,
+    War,
+    Fantasy,
+}
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+enum LibraryPackages {
+    None,
+    Basic,
+    Premium,
+}
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    // SETUP CONTRACT STATE
+struct Location {
+    drawer: u8,
+    column: u8,
 }
 
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
 #[near_bindgen]
-impl Contract {
-    // ADD CONTRACT METHODS HERE
+struct Books {
+    id: u16,
+    name: String,
+    available: bool,
+    copies: u16,
+    category: BookCategories,
+    location: Location,
 }
 
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+#[near_bindgen]
+struct BooksBorrowed {
+    book_id: u16,
+    date_borrowed: String,
+    date_due: String,
+}
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+#[near_bindgen]
+struct Users {
+    name: String,
+    id: u8,
+    is_member: bool,
+    member_package: LibraryPackages,
+    books_taken: Vec<BooksBorrowed>,
+}
+#[near_bindgen]
+impl Default for Users {
+    fn default() -> Self {
+        Users {
+            name: "".to_string(),
+            id: 0,
+            is_member: false,
+            member_package: LibraryPackages::None,
+            books_taken: vec![],
+        }
+    }
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+#[near_bindgen]
+struct Library {
+    users: Vec<Users>,
+    books: Vec<Books>,
+}
+#[near_bindgen]
+impl Default for Library {
+    fn default() -> Self {
+        Library {
+            users: vec![],//Vector::new(b"r".to_vec()),
+            books: vec![]//::new(b"r".to_vec()),
+        }
+    }
+}
+
+impl Library {
+    pub fn get_books_length(&self)->u8 {
+   self.books.len() as u8
+    }
+    pub fn add_book(&mut self, name: String, category: String, drawer: u8, column: u8) {
+        let bk = Books {
+            id: self.books.len() as u16 + 1,
+            name: name,
+            available: true,
+            copies: 1,
+            category: match category.as_str() {
+                "war" => BookCategories::War,
+                _ => BookCategories::Any,
+            },
+            location: Location {
+                drawer: drawer,
+                column: column,
+            },
+        };
+        self.books.push(bk)
+    }
+
+    pub fn  add_user(&mut self,name:String ){
+        let tmp = self.books.len() as u8 + 1;
+        let user = Users {
+            name: name,
+            id: tmp,
+            is_member: false,
+            member_package: LibraryPackages::None,
+            books_taken: vec![],
+        };
+        self.users.push(user);
+    }
+    pub fn borrow_book(&mut self, book_name:String, user_name:String){
+// check add user and borrow book function
+        let mut bok:Option<&Books> = None;
+
+
+        for elem in self.books.iter() {
+            if elem.name == book_name{
+                //elem.available = false;
+                bok = Some (elem);
+            }
+        }
+        match bok {
+            Some(bk)=>{
+                for elem in self.users.iter_mut() {
+                    if elem.name == user_name{
+    
+                        let bk_borrowed = BooksBorrowed{
+                            book_id: bk.id,
+                            date_borrowed: "20/aug/2022".to_string(),
+                            date_due: "27/aug/2022".to_string(),
+                        }; 
+        
+                        elem.books_taken.push (bk_borrowed)
+        
+        
+                    }
+                }
+        
+               }
+               None =>{
+                panic!("Book not found ")
+               }
+           }
+    
+            
+    
+        }
+    }
+                
+                    
 
 // use the attribute below for unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
-
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
+ 
+ 
+    // TESTS HERE
+    #[test]
+    fn add_book() {
+       
+        let mut library = Library::default();
+        library.add_book("IT Manual".to_string(), "manuals".to_string(), 8, 3);
+        assert_eq!(library.get_books_length(), 1);
     }
 
-    // TESTS HERE
+    #[test]
+    fn add_user() {
+       
+        let mut library = Library:: default();
+        library.add_user("Frank".to_string());
+        assert_eq!(library.users.len(), 1);
+    }
+
+    #[test]
+ 
+    fn borrow_book() {
+      
+        let mut library = Library::default();
+        library.add_book("IT Manual".to_string(), "manuals".to_string(),8, 3);
+        library.add_user("Frank".to_string());
+        library.borrow_book("IT Manual".to_string(), "Frank".to_string());
+        
+        let mut book_length =0;
+        for elem in  library.users.iter() {
+            if elem.name == "Frank".to_string(){
+                book_length = elem.books_taken.len();
+                break;
+                        }
+        }
+        assert_eq!(book_length, 1)
+    }
+   
 }
+
